67765b763b83b22db4f86d08ba63a4f1ffcfa501
adityaiiitmk <aditya.s@duk.ac.in>
2023-09-24 00:01:37 +0530

    Renaming

--- Perceptron/perceptron_trainer.py
+++ Perceptron/perceptron_trainer.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
     y = np.array([0, 1, 1, 1])  
 
-    perceptron = Perceptron(epochs=1)
+    perceptron = Perceptron(epochs=10)
     perceptron.fit(X, y)
 
     test_data = np.array([[0, 1], [1, 0], [0, 0], [1, 0]])
