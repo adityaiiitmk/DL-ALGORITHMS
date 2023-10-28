317afaeffbdfe62cb5ee211e324a148c06c38201
adityaiiitmk <aditya.s@duk.ac.in>
2023-10-28 15:13:04 +0530

    Version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-__version__ = "0.0.1"
+__version__ = "0.0.3"
 
 REPO_NAME = "DL-ALGORITHMS"
 AUTHOR_USER_NAME = "adityaiiitmk"
